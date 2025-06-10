bacb12cb9876f65d2bc119c39f7ede038e6d74a8
yuki-1184 <yuki.1184apple@gmail.com>
2025-06-10 12:57:57 +0900

    LayerNormalization

--- MyTransformers/layers/feed_forward_network.py
+++ MyTransformers/layers/feed_forward_network.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 
 class FeedForwardNetwork(nn.Module):
-    def __init__(self, d_model: int, d_ff: int = 2048, dropout: float = 0.1):
+    def __init__(self, d_model: int, d_ff: int, dropout: float = 0.1):
         super().__init__()
+        # d_ff is usually 4 * hidden_dim(d_model)
         self.linear1 = nn.Linear(d_model, d_ff)
         self.linear2 = nn.Linear(d_ff, d_model)
         self.relu = nn.ReLU()
---
+++ MyTransformers/layers/layer_normalization.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+
+class LayerNormalization(nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-6):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.ones(d_model))  # 学習可能なスケーリングパラメータ -> 1.0
+        self.beta = nn.Parameter(torch.zeros(d_model))  # 学習可能なバイアスパラメータ -> 0.0
+        self.eps = eps
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        mean = x.mean(-1, keepdim=True)
+        var = x.var(-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) * torch.sqrt(var + self.eps)
+        return self.gamma * norm_x + self.beta
+
+
+"""
+層正規化
+過剰に大きい値によって勾配が消失するのを防ぐために、各層の出力を正規化します。
+層正規化は、Transformerの各層で使用され、入力テンソルの各要素をその平均と分散で正規化します。
+
+🔸 gamma = 1（スケーリング初期値）
+	•	正規化後の出力は平均0・分散1のデータになります。
+	•	ここに gamma=1 をかけると「そのままのスケール」を維持する。
+	•	→ 正規化後の値を変えずに出力に流せる。
+
+🔸 beta = 0（バイアス初期値）
+	•	出力にバイアスを加えない（平均0のまま）。
+	•	→ 非正規化方向への変化は起きない。
+"""
